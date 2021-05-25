030f4b9b9c4f1a2ecb69ebd442d8309d5eac11c0
sandres-unizar <79507425+sandres-unizar@users.noreply.github.com>
2021-05-25 19:47:35 +0200

    update project

--- application/src/main/java/com/example/multimodule/application/ChatController.java
+++ application/src/main/java/com/example/multimodule/application/ChatController.java
@@ -3,6 +3,7 @@ package com.example.multimodule.application;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.stereotype.Controller;
+import com.example.fileupload.*;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -13,9 +14,13 @@ public class ChatController {
     @Autowired
     private Runner runner;
 
+    @Autowired
+    private MessageDBRepository messageDBRepository;
+
     @MessageMapping("/chat")
     public OutputMessage send(final JsonMessage jsonMessage) throws Exception {
         System.out.println("Received: " + jsonMessage.getFrom() + " -> " + jsonMessage.getText());
+        messageDBRepository.save(new MessageDB(jsonMessage.getFrom(), jsonMessage.getText(),new Date().toString()));
         runner.run(jsonMessage.getFrom() + ":::" + jsonMessage.getText());
         final String time = new SimpleDateFormat("HH:mm").format(new Date());
         return new OutputMessage(jsonMessage.getFrom(), jsonMessage.getText(), time);
@@ -24,6 +29,7 @@ public class ChatController {
     @MessageMapping("/chatRoom")
     public OutputMessage outputMessage(final JsonMessage jsonMessage) throws Exception{
         System.out.println("Received: " + jsonMessage.getFrom() + " -> " + jsonMessage.getText());
+        messageDBRepository.save(new MessageDB(jsonMessage.getFrom(), jsonMessage.getText(),new Date().toString()));
         runner.toChatRoom(jsonMessage.getFrom() + ":::" + jsonMessage.getText());
         final String time = new SimpleDateFormat("HH:mm").format(new Date());
         return new OutputMessage(jsonMessage.getFrom(), jsonMessage.getText(), time);
